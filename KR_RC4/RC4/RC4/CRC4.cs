b05876dce76ff047f160cec1b51e1fbdb6099d0e
Victria <petrochenkovictoria@gmail.com>
2019-09-23 22:40:17 +0300

    Course

---
+++ KR_RC4/RC4/RC4/CRC4.cs
@@ -0,0 +1,83 @@
+﻿namespace RC4
+{
+    static class SwapElem
+    {
+        // Функция, которая меняет местами i1-й и i2-й элементы массива array
+        public static void Swap<T>(this T[] array, int i1, int i2)
+        {
+            T temp = array[i1];
+            array[i1] = array[i2];
+            array[i2] = temp;
+        }
+    }
+    public class CRC4
+    {
+        private byte[] S; //S-блок - массив
+        private int length; //Размер S-блока - размер массива
+        private int ii = 0; //параметр для метода PRGA
+        private int jj = 0; //параметр для метода PRGA
+
+        // Конструктор с параметром для создания массива из 2^n элеиентов
+        public CRC4(int n)
+        {
+            length = 1 << n;
+            S = new byte[length];
+        }
+
+        // Метод, который реализует заполнение S-блока начальными значениями
+        private void FillS()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                S[i] = (byte)i;
+            }
+        }
+
+        // Метод, который реализует алгоритм KSA
+        private void KSA(byte[] key)
+        {
+            FillS();
+            int keyLength = key.Length;
+            int j = 0;
+            for (int i = 0; i < length; i++)
+            {
+                j = (j + S[i] + key[i%keyLength]) % length;
+                S.Swap(i, j);
+            }
+        }
+
+        // Метод, который реализует алгоритм PRGA
+        private byte PRGA()
+        {
+            ii = (ii + 1) % length;
+            jj = (jj + S[ii]) % length;
+            S.Swap(ii, jj);
+            int t = (S[ii] + S[jj]) % length;
+            return S[t];
+        }
+
+        // Метод зашифровать
+        public byte[] Encode(byte[] openT, byte[] key)
+        {
+            KSA(key);
+            byte[] closeT = new byte[openT.Length];
+            for (int m = 0; m < openT.Length; m++)
+            {
+                closeT[m] = (byte)(openT[m] ^ PRGA());
+            }
+            return closeT;
+        }
+
+        // Метод дешифровать
+        public byte[] Decode(byte[] closeT, byte[] key)
+        {
+            KSA(key);
+            byte[] openT = new byte[closeT.Length];
+            for (int m = 0; m < openT.Length; m++)
+            {
+                openT[m] = (byte)(closeT[m] ^ PRGA());
+            }
+            return openT;
+        }
+    }
+}
---
+++ KR_RC4/TestRC4/TestRC4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RC4;
+
+namespace TestRC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введить ключ для шифрования");
+            string mykey1 = Console.ReadLine();
+
+            Console.WriteLine("Введить сообщение, которое хотите зашифровать");
+            string myOpenText = Console.ReadLine();
+
+            byte[] key1 = ASCIIEncoding.ASCII.GetBytes(mykey1);
+            byte[] openText = ASCIIEncoding.ASCII.GetBytes(myOpenText);
+
+            CRC4 encodeRC4 = new CRC4(8);
+
+            byte[] closeText = encodeRC4.Encode(openText,key1);
+            string myCloseText = ASCIIEncoding.ASCII.GetString(closeText);
+
+            Console.WriteLine("Сообщение зашифровано");
+            Console.WriteLine(myCloseText);
+
+            CRC4 decodeRC4 = new CRC4(8);
+
+            Console.WriteLine("Введите ключ для расшифровки");
+            string mykey2 = Console.ReadLine();
+
+            byte[] key2 = ASCIIEncoding.ASCII.GetBytes(mykey2);
+
+            byte[] newOpenText = decodeRC4.Decode(closeText,key2);
+
+            string myNewOpenText = ASCIIEncoding.ASCII.GetString(newOpenText);
+
+            Console.WriteLine("Сообщение расшифровано");
+            Console.WriteLine(myNewOpenText);
+
+         
+            Console.ReadLine();
+
+        }
+    }
+}
